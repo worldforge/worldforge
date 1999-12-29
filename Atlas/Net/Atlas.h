d9064be5e36684bd811f02b9a7c7bce034e5295b
John Barrett <jbarret@qx.net>
1999-12-29 15:48:37 +0000

    fixing more commit messups

---
+++ Atlas/Net/Atlas.h
@@ -0,0 +1,16 @@
+
+#ifndef _ATLAS_H
+#define _ATLAS_H
+
+#include "AtlasDebug.h"
+#include "AtlasClient.h"
+#include "AtlasCodec.h"
+#include "AtlasObject.h"
+#include "AtlasProtocol.h"
+#include "AtlasPackedAsciiProtocol.h"
+#include "AtlasXMLProtocol.h"
+#include "AtlasServer.h"
+#include "AtlasSocket.h"
+#include "AtlasTCPSocket.h"
+
+#endif
