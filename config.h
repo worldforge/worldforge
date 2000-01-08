f2cc0ca3130928376e81f3668719c53ce82da3ac
Michael Day <mikeday@corplink.com.au>
2000-01-08 10:02:51 +0000

    Beginning use of automike on Atlas.

---
+++ config.h
@@ -0,0 +1,9 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "Atlas"
+
+/* Version number of package */
+#define VERSION "0.1.0"
+
