62bd5e297ca87667a081efcbc40cde8395c6aa29
Michael Day <mikeday@corplink.com.au>
2000-02-16 10:48:40 +0000

    Adding Atlas::Stream

---
+++ Atlas/Stream/Stream.cc
@@ -0,0 +1,4 @@
+// This file is distributed under the GNU Lesser General Public license.
+// Copyright (C) 2000 Michael Day (See the file COPYING for details).
+
+#include "Stream.h"
---
+++ Atlas/Stream/Stream.h
@@ -0,0 +1,9 @@
+// This file is distributed under the GNU Lesser General Public license.
+// Copyright (C) 2000 Michael Day (See the file COPYING for details).
+
+#ifndef ATLAS_STREAM_STREAM_H
+#define ATLAS_STREAM_STREAM_H
+
+
+
+#endif
