d9d09a45eff69c005a9289279f027c146d4fcd26
Michael Day <mikeday@corplink.com.au>
2000-02-16 11:17:20 +0000

    Removing obsolete files

--- Atlas/Stream/Stream.cc
+++ Atlas/Stream/Stream.cc
@@ -2,3 +2,27 @@
 // Copyright (C) 2000 Michael Day (See the file COPYING for details).
 
 #include "Stream.h"
+	
+void Atlas::Stream::Begin(const std::string& name, Type)
+{
+}
+
+void Atlas::Stream::End()
+{
+}
+	
+void Atlas::Stream::Send(const std::string& name, int value)
+{
+}
+
+void Atlas::Stream::Send(const std::string& name, float value)
+{
+}
+
+void Atlas::Stream::Send(const std::string& name, const std::string& value)
+{
+}
+
+void Atlas::Stream::Send(const std::string& name, const Object& value)
+{
+}
--- Atlas/Stream/Stream.h
+++ Atlas/Stream/Stream.h
@@ -4,6 +4,28 @@
 #ifndef ATLAS_STREAM_STREAM_H
 #define ATLAS_STREAM_STREAM_H
 
+#include "../Object/Object.h"
 
+#include <string>
+
+namespace Atlas
+{
+    namespace Stream
+    {
+	enum Type
+	{
+	    LIST,
+	    MAP,
+	};
+	
+	void Begin(const std::string& name, Type);
+	void End();
+	
+	void Send(const std::string& name, int value);
+	void Send(const std::string& name, float value);
+	void Send(const std::string& name, const std::string& value);
+	void Send(const std::string& name, const Object& value);
+    }
+}
 
 #endif
