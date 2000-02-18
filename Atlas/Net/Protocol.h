090ab73d630ed21dc4c3e76c4cb6b443efaeb854
Aloril <aloril@iki.fi>
2000-02-18 10:23:55 +0000

    fixed typo: =0 was not removed when method was added

--- Atlas/Net/Protocol.h
+++ Atlas/Net/Protocol.h
@@ -46,7 +46,7 @@ public:
 	const string&       getPrefix() const { return prefix; }
 	void                setPrefix(  const string& s)	{ prefix = s; }
 	
-    virtual	Protocol*	makenew()=0; // { 
+    virtual	Protocol*	makenew() { 
     	assert( false ); return 0; 
 	/*should not be called on base class! - fex*/ 
 	}
