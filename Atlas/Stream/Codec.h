78471d52b4827a12185bcd17d91981184406588c
Michael Day <mikeday@corplink.com.au>
2000-02-21 00:56:05 +0000

    Beginning new stream architecture

--- Atlas/Stream/Codec.h
+++ Atlas/Stream/Codec.h
@@ -1,8 +1,9 @@
-// This file is distributed under the GNU Lesser General Public license.
-// Copyright (C) 2000 Michael Day (See the file COPYING for details).
+// This file may be redistributed and modified under the terms of the
+// GNU Lesser General Public License (See COPYING for details).
+// Copyright (C) 2000 Michael Day
 
-#ifndef ATLAS_STREAM_STREAM_H
-#define ATLAS_STREAM_STREAM_H
+#ifndef ATLAS_STREAM_CODEC_H
+#define ATLAS_STREAM_CODEC_H
 
 #include "../Object/Object.h"
 
