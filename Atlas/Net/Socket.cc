e7f768dbad55dd657287b36a6ae670ba1234e04a
Michael Day <mikeday@corplink.com.au>
2000-04-28 14:16:45 +0000

    Removing Atlas::Net

--- Atlas/Net/Socket.cc
+++
@@ -1,9 +0,0 @@
-// This file may be redistributed and modified only under the terms of
-// the GNU Lesser General Public License (See COPYING for details).
-// Copyright (C) 2000 Michael Day
-
-#include "Socket.h"
-
-Atlas::Net::Socket::~Socket()
-{
-}
