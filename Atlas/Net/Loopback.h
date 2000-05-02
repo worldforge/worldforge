8727598dacfcf8aad0a20d29499635fd763e82bb
Michael Day <mikeday@corplink.com.au>
2000-05-02 12:01:37 +0000

    Added loopback support

---
+++ Atlas/Net/Loopback.h
@@ -0,0 +1,16 @@
+// This file may be redistributed and modified only under the terms of
+// the GNU Lesser General Public License (See COPYING for details).
+// Copyright (C) 2000 Michael Day
+
+#ifndef ATLAS_NET_LOOPBACK_H
+#define ATLAS_NET_LOOPBACK_H
+
+#include "../Bridge.h"
+
+namespace Atlas {
+
+void Loopback(Bridge*, Bridge*, Bridge*&, Bridge*&);
+
+} // Atlas namespace
+
+#endif
