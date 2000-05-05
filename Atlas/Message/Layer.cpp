82356817f648c015f9b452933c24be358be711e2
Stefanus Du Toit <sdt@gmx.net>
2000-05-05 16:38:30 +0000

    Beginnings of a Message::Layer.
    Mike check out the comments and get back to me about that.

--- Atlas/Message/Layer.cpp
+++ Atlas/Message/Layer.cpp
@@ -81,3 +81,54 @@ inline void recurseListObject(const Object& obj, Bridge* b)
     }
 }
 
+inline void recurseStreamObject(const Object& obj, Bridge* b)
+{
+    if (obj.isMap()) {
+        b->StreamMessage(Bridge::MapBegin);
+        Object names = obj.keys();
+        Object values = obj.vals();
+        for (size_t i = 0; i < names.length(); i++) {
+            string itemName;
+            Object value;
+            names.get(i, itemName);
+            values.get(i, value);
+            recurseMapObject(value, b, itemName);
+        }
+        b->MapEnd();
+    } else {
+        // FIXME - report error
+    }
+}
+
+Layer::Layer(Atlas::Bridge* b) : bridge(b), complete(false)
+{
+}
+
+void Layer::StreamMessage(const Object& obj)
+{
+    recurseStreamObject(obj, bridge);
+}
+
+void Layer::MapItem(const string& name, const Object& obj)
+{
+    recurseMapObject(obj, bridge, name);
+}
+
+void Layer::ListItem(const Object& obj)
+{
+    recurseListObject(obj, bridge);
+}
+
+void Layer::ReceiveObject(Object& target)
+{
+    while (!complete) { }
+    target = GetObject();
+}
+
+Object Layer::GetObject()
+{
+    Object old = object;
+    object = Object();
+    complete = false;
+    return old;
+}
--- Atlas/Message/Layer.h
+++ Atlas/Message/Layer.h
@@ -14,19 +14,48 @@ namespace Atlas { namespace Message {
 
 FIXME document this
 
+XXX sdt --
+
+some things I'm not sure about - this is meant to be symmetric, right, so
+you use Layer for both a Bridge that acts as receiver and for a sender.
+
+But if so, then that's a bit icky when it comes to the Object& item methods
+being for sending but the other item methods for receiving..
+
+What about layers that want to override the same methods for both retrieval and
+sending?
+
+Tell me whether I'm missing something somewhere :)
+
+-- sdt XXX
+
 @see Object
 @see Bridge
 */
 
 class Layer : public Atlas::Bridge
 {
-    public:
+public:
 
     Layer(Atlas::Bridge*);
 
-    private:
+    virtual ~Layer() { }
+
+    virtual void StreamMessage(const Object& obj);
+    virtual void MapItem(const string& name, const Object& obj);
+    virtual void ListItem(const Object& obj);
+
+    virtual void ReceiveObject(Object& target);
+
+protected:
+
+    Object GetObject();
+    
+private:
 
     Atlas::Bridge* bridge;
+    Object object;
+    bool complete;
 };
 
 } } // Atlas::Message namespace
