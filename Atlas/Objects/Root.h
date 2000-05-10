5e7e500efc4be25259d3815e8c15e43c039c0d30
Stefanus Du Toit <sdt@gmx.net>
2000-05-10 13:43:47 +0000

    Removed Root functionality to be replaced.

--- Atlas/Objects/Root.h
+++ Atlas/Objects/Root.h
@@ -5,40 +5,11 @@
 #ifndef ATLAS_OBJECTS_ROOT_H
 #define ATLAS_OBJECTS_ROOT_H
 
-#include <string>
-#include "ObjectType.h"
-
 namespace Atlas { namespace Objects {
 
 class Root
 {
-public:
-    Root(const string& id, const string& name);
-
-    virtual ~Root();
-
-    std::string GetId();
-    void SetId(const std::string& id);
-    void RemoveId();
-    bool HasId();
     
-    std::string GetName();
-    void SetName(const std::string& name);
-    void RemoveName();
-    bool HasName();
-    
-    ObjectType GetObjectType();
-    void SetObjectType(ObjectType type);
-    bool HasObjectType() { return true; }
-
-    void Transmit(Bridge* b);
-    
-protected:
-    
-    std::string* id;
-    std::string* name;
-    
-    ObjectType objectType;
 };
 
 } } // namespace Atlas::Objects
