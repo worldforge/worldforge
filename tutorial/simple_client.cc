c4e71995772e9411fda3e12fff47d34b270fc407
Stefanus Du Toit <sdt@gmx.net>
2000-05-30 08:55:39 +0000

    Documented the .cc files.

--- tutorial/simple_client.cc
+++ tutorial/simple_client.cc
@@ -7,17 +7,21 @@
  * This file is covered by the GNU Free Documentation License.
  */
 
+// cout, cerr
 #include <iostream>
+// sleep()
 #include <unistd.h>
+// iosockinet - the iostream-based socket class
 #include <sockinet.h>
-#include <Atlas/Bridge.h>
-#include <Atlas/Negotiate.h>
+// Atlas negotiation
 #include <Atlas/Net/Stream.h>
+// The DebugBridge
 #include "DebugBridge.h"
 
 using namespace Atlas;
 using namespace std;
 
+// This sends a very simple message to c
 void helloWorld(Codec<iostream>& c)
 {
     cout << "Sending hello world message... " << flush;
@@ -29,43 +33,56 @@ void helloWorld(Codec<iostream>& c)
 
 int main(int argc, char** argv)
 {
+    // The socket that connects us to the server
     iosockinet stream(sockbuf::sock_stream);
 
     cout << "Connecting..." << flush;
     
+    // Connect to the server
     stream->connect("127.0.0.1", 6767);
     
+    // The DebugBridge puts all that comes through the codec on cout
     DebugBridge bridge;
+    // Do client negotiation with the server
     Net::StreamConnect conn("simple_client", stream, &bridge);
 
     cout << "Negotiating... " << flush;
+    // conn.Poll() does all the negotiation
     while (conn.GetState() == Negotiate<iostream>::IN_PROGRESS) {
         conn.Poll();
     }
-
     cout << "done" << endl;
 
+    // Check whether negotiation was successful
     if (conn.GetState() == Negotiate<iostream>::FAILED) {
         cerr << "Failed to negotiate" << endl;
         return 2;
     }
+    // Negotiation was successful
 
+    // Get the codec that negotiation established
     Codec<iostream>* codec = conn.GetCodec();
 
+    // This should always be sent at the beginning of a session
     codec->StreamBegin();
     
+    // Say hello to the server
     helloWorld(*codec);
-
     stream << flush;
 
     cout << "Sleeping for 2 seconds... " << flush;
+    // Sleep a little
     sleep(2);
     cout << "done." << endl;
 
+    // iosockinet::operator bool() returns false if the connection was broken
     if (!stream) cout << "Server exited." << endl; else {
+        // It was not broken by the server, so we'll close ourselves
         cout << "Closing connection... " << flush;
+        // This should always be sent at the end of a session
         codec->StreamEnd();
         stream << flush;
+        // Close the socket
         stream->close();
         cout << "done." << endl;
     }
--- tutorial/simple_server.cc
+++ tutorial/simple_server.cc
@@ -7,9 +7,13 @@
  * This file is covered by the GNU Free Documentation License.
  */
 
+// cout, cerr
 #include <iostream>
+// isockinet, iosockinet - the iostream socket classes
 #include <sockinet.h>
+// Atlas negotiation
 #include <Atlas/Net/Stream.h>
+// The DebugBridge
 #include "DebugBridge.h"
 
 using namespace Atlas;
@@ -17,38 +21,52 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
+    // This socket accepts connections
     isockinet is(sockbuf::sock_stream);
     
+    // Bind the socket. 0.0.0.0 accepts on any incoming address
     is->bind("0.0.0.0", 6767);
     cout << "Bound to " << is->localhost() << ":" << is->localport() << endl;
     
+    // Listen for clients
     is->listen();
     cout << "Listening... " << flush;
 
+    // This blocks until a client connects
     iosockinet client(is->accept());
     cout << "accepted client connection!" << endl;
 
+    // The DebugBridge puts all that comes through the codec on cout
     DebugBridge bridge;
+    // Do server negotiation for Atlas with the new client
     Net::StreamAccept accepter("simple_server", client, &bridge);
 
     cout << "Negotiating.... " << flush;
+    // accepter.Poll() does all the negotiation
     while (accepter.GetState() == Net::StreamAccept::IN_PROGRESS)
         accepter.Poll();
     cout << "done." << endl;
 
+    // Check the negotiation state to see whether it was successful
     if (accepter.GetState() == Net::StreamAccept::FAILED) {
         cerr << "Negotiation failed." << endl;
         return 2;
     }
+    // Negotiation was successful! 
 
+    // Get the codec that negotation established
     Codec<iostream>* codec = accepter.GetCodec();
 
     cout << "Polling client..." << endl;
     
+    // iosockinet::operator bool() returns false once a connection closes
     while (client) {
+        // Get incoming data and process it
         codec->Poll(); // this blocks!
     }
 
+    // The connection closed
+    
     cout << "Client exited." << endl;
 
     return 0;
