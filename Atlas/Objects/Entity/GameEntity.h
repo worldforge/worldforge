8d318fde14196099a70d6a3ee5f25f4f6f5ab626
Stefanus Du Toit <sdt@gmx.net>
2000-06-12 17:19:33 +0000

    Forgot to add this.

---
+++ Atlas/Objects/Entity/GameEntity.h
@@ -0,0 +1,24 @@
+// This file may be redistributed and modified only under the terms of
+// the GNU Lesser General Public License (See COPYING for details).
+// Copyright 2000 Stefanus Du Toit.
+// Automatically generated using gen_cc.py.
+
+#ifndef ATLAS_OBJECTS_ENTITY_GAMEENTITY_H
+#define ATLAS_OBJECTS_ENTITY_GAMEENTITY_H
+
+#include "RootEntity.h"
+
+namespace Atlas { namespace Objects { namespace Entity { 
+
+class GameEntity : public RootEntity
+{
+public:
+    GameEntity();
+    virtual ~GameEntity() { }
+
+protected:
+};
+
+} } } // namespace Atlas::Objects::Entity
+
+#endif // ATLAS_OBJECTS_ENTITY_GAMEENTITY_H
