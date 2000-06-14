d4c15da9ef6a250ee0bb022d29728b6461367acd
Stefanus Du Toit <sdt@gmx.net>
2000-06-14 09:24:29 +0000

    Now it's a _real_ test (included in TESTS)

--- tests/Objects/objects1.cc
+++ tests/Objects/objects1.cc
@@ -1,31 +1,47 @@
 #include <iostream>
+#include <cassert>
 #include <Atlas/Message/Object.h>
 #include <Atlas/Objects/Root.h>
 #include <Atlas/Objects/Operation/Look.h>
+#include <Atlas/Objects/Entity/Account.h>
 
 using namespace Atlas;
 using namespace Atlas::Objects;
 using namespace std;
-
-void list_parents(const Root* r)
-{
-    typedef Message::Object::ListType::const_iterator Iter;
-    
-    cout << r->GetAttr("id").AsString() << " parents:" << endl;
-    for (Iter I = r->GetAttr("parent").AsList().begin();
-            I != r->GetAttr("parent").AsList().end(); I++)
-        cout << (*I).AsString() << endl;
-}
 
 int main(int argc, char** argv)
 {
     Root root;
     Root root_inst = Root::Instantiate();
     root_inst.SetAttr("id", string("root_instantiation"));
-    list_parents(&root); list_parents(&root_inst);
+    assert(root.GetAttr("id").AsString() == "root");
+    assert(root_inst.GetAttr("id").AsString() == "root_instantiation");
+    assert(root.GetAttr("parent").AsList().size() == 0);
+    assert(root_inst.GetAttr("parent").AsList().size() == 1);
+    assert((*root_inst.GetAttr("parent").AsList().begin()).AsString() ==
+            "root");
 
     Operation::Look look;
     Operation::Look look_inst = Operation::Look::Instantiate();
     look_inst.SetAttr("id", string("look_instantiation"));
-    list_parents(&look); list_parents(&look_inst);
+    assert(look.GetAttr("id").AsString() == "look");
+    assert(look_inst.GetAttr("id").AsString() == "look_instantiation");
+    assert(look.GetAttr("parent").AsList().size() == 1);
+    assert((*look.GetAttr("parent").AsList().begin()).AsString() ==
+            "perceive");
+    assert(look_inst.GetAttr("parent").AsList().size() == 1);
+    assert((*look_inst.GetAttr("parent").AsList().begin()).AsString() ==
+            "look");
+
+    Entity::Account acct;
+    Entity::Account acct_inst = Entity::Account::Instantiate();
+    acct_inst.SetAttr("id", string("account_instantiation"));
+    assert(acct.GetAttr("id").AsString() == "account");
+    assert(acct_inst.GetAttr("id").AsString() == "account_instantiation");
+    assert(acct.GetAttr("parent").AsList().size() == 1);
+    assert((*acct.GetAttr("parent").AsList().begin()).AsString() ==
+            "admin_entity");
+    assert(acct_inst.GetAttr("parent").AsList().size() == 1);
+    assert((*acct_inst.GetAttr("parent").AsList().begin()).AsString() ==
+            "account");
 }
