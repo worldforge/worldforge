1b770c7e67175d81c661798967a718cbda4cb30c
jaz147 <jaz147>
2001-05-19 17:06:26 +0000

    * Changed varconf license to LGPL.

--- varconf/parse_error.cpp
+++ varconf/parse_error.cpp
@@ -1,31 +1,28 @@
 /*
  *  parse_error.cpp - implementation of the parse error handling class
- *  Copyright (C) 2000, Stefanus Du Toit, Joseph Zupko
+ *  Copyright (C) 2001, Stefanus Du Toit, Joseph Zupko
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
+ *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- *  For more information on the GPL, please go to: 
- *  http://www.gnu.org/copyleft/gpl.html
+ *  Contact:  Joseph Zupko
+ *            jaz147@psu.edu
  *
- *      Contact:  Joseph Zupko
- *                jaz147@psu.edu
- *
- *                189 Reese St.
- *                Old Forge, PA 18518
+ *            189 Reese St.
+ *            Old Forge, PA 18518
  */
-
+ 
 #include <cstdio>
 #include <iostream>
 #include "parse_error.h"
