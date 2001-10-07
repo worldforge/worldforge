024a88a3412d387572ae9cf97a8bb4b864be4aff
Jesse Jones <jesjones@mindspring.com>
2001-10-07 07:32:25 +0000

    Made the BaseObject.h include the first include to ensure that the header is stand-alone.

--- Atlas/Objects/BaseObject.cpp
+++ Atlas/Objects/BaseObject.cpp
@@ -2,8 +2,9 @@
 // the GNU Lesser General Public License (See COPYING for details).
 // Copyright (C) 2000-2001 Stefanus Du Toit and Aloril
 
-#include "../Message/Encoder.h"
 #include "BaseObject.h"
+
+#include "../Message/Encoder.h"
 
 using Atlas::Message::Object;
 
