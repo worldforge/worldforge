ca2226bd0d9bd0a9b7d7d18a45266f9d8443b48f
James Turner <james@worldforge.org>
2002-02-11 22:03:08 +0000

    Monday, February 11 2002, James Turner
    	* Implemented Player::getAccountID();

--- Eris/Player.cpp
+++ Eris/Player.cpp
@@ -254,6 +254,11 @@ void Player::internalLogin(const std::string &uname, const std::string &pwd)
 	_currentSerial = l.GetSerialno();
 }
 
+std::string Player::getAccountID() const
+{
+    return _account;
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // callbacks
 
