8501b0513321386f1e7439ff7759d9edf80c19ac
Ron Steinke <rsteinke@w-link.net>
2002-04-17 17:27:34 +0000

    * Changed Time::Stamp from a typedef'd struct to a full 	  class, operator-(const Stamp&, const Stamp&) is now 	  recognized properly

--- Eris/Time.cpp
+++ Eris/Time.cpp
@@ -4,24 +4,16 @@
 
 #include "Time.h"
 
-namespace Time
-{
-
 #include <unistd.h>	
 #include <sys/time.h>
- 
-	/*
-void Init()
-{
-	gettimeofday(&_startTime, NULL);
-}
-	*/
+
+bool Time::Stamp::_did_init = false;
 	
-Stamp getCurrentStamp()
+Time::Stamp Time::getCurrentStamp()
 {
 	Stamp n;
 #ifndef __WIN32__
-	gettimeofday(&n, NULL);
+	gettimeofday(&n._val, NULL);
 #else
   SYSTEMTIME sysTime;
   FILETIME fileTime;  /* 100ns == 1 */
@@ -33,55 +25,55 @@ Stamp getCurrentStamp()
    * FILETIME. */
   memcpy(&i, &fileTime, sizeof(LARGE_INTEGER));
 
-  n.tv_sec = i.QuadPart / 10000000; /*10e7*/
-  n.tv_usec = (i.QuadPart / 10) % 1000000;  /*10e6*/
+  n._val.tv_sec = i.QuadPart / 10000000; /*10e7*/
+  n._val.tv_usec = (i.QuadPart / 10) % 1000000;  /*10e6*/
                                   
 #endif
 	return n;
 }
+
+// The operator definitons need the Time:: prefix, since
+// they were declared in that namespace
 	
-bool operator<(const Stamp &a, const Stamp &b)
+bool Time::operator<(const Time::Stamp &a, const Time::Stamp &b)
 {
-	if (a.tv_sec == b.tv_sec)
-		return (a.tv_usec < b.tv_usec);
+	if (a._val.tv_sec == b._val.tv_sec)
+		return (a._val.tv_usec < b._val.tv_usec);
 	else
-		return a.tv_sec < b.tv_sec;
+		return a._val.tv_sec < b._val.tv_sec;
 }
 
-Stamp operator+(const Stamp &a, unsigned long msec)
+Time::Stamp Time::operator+(const Time::Stamp &a, unsigned long msec)
 {
 	Stamp ret = a;
-	ret.tv_sec += msec / 1000;
-	ret.tv_usec += (msec % 1000) * 1000;
+	ret._val.tv_sec += msec / 1000;
+	ret._val.tv_usec += (msec % 1000) * 1000;
 	
-	if (ret.tv_usec > 1000000) {
-		ret.tv_sec++;
-		ret.tv_usec-=1000000;
+	if (ret._val.tv_usec > 1000000) {
+		ret._val.tv_sec++;
+		ret._val.tv_usec-=1000000;
 	}
 	
 	return ret;
 }
 
-Stamp operator-(const Stamp &a, unsigned long msec)
+Time::Stamp Time::operator-(const Time::Stamp &a, unsigned long msec)
 {
 	Stamp ret = a;
-	ret.tv_sec -= msec / 1000;
-	ret.tv_usec -= (msec % 1000) * 1000;
+	ret._val.tv_sec -= msec / 1000;
+	ret._val.tv_usec -= (msec % 1000) * 1000;
 	
-	if (ret.tv_usec < 0) {
-		ret.tv_sec--;
-		ret.tv_usec+=1000000;
+	if (ret._val.tv_usec < 0) {
+		ret._val.tv_sec--;
+		ret._val.tv_usec+=1000000;
 	}
 	
 	return ret;
 }
 
-long operator-(const Stamp &a, const Stamp &b)
+long Time::operator-(const Time::Stamp &a, const Time::Stamp &b)
 {
-	unsigned long ret = (a.tv_sec - b.tv_sec) * 1000;
-	ret += (a.tv_usec - b.tv_usec) / 1000;
+	unsigned long ret = (a._val.tv_sec - b._val.tv_sec) * 1000;
+	ret += (a._val.tv_usec - b._val.tv_usec) / 1000;
 	return ret;
-}
-
-	
 }
--- Eris/Time.h
+++ Eris/Time.h
@@ -16,29 +16,45 @@ extern "C" {
 #endif
 
 namespace Time {
+
+class Stamp {
+ private:
 #if defined( __WIN32__ )
 	
 	// We roll our own timeval... may only need to be done for mingw32.
-	typedef struct eris_timeval Stamp;
+	// FIXME since we're no longer doing a typedef, do we really need to do this?
+	struct eris_timeval _val;
 
 #elif defined( macintosh )
-	typedef UnsignedWide Stamp;	// micro-seconds
+	UnsignedWide _val;	// micro-seconds
 #else
 	// POSIX, BeOS, ....
-	typedef struct timeval Stamp;
+	struct timeval _val;
 #endif	
 
-bool operator<(const Stamp &a, const Stamp &b);
+	// Hook for future expansion to other platforms,
+	// in case the timer needs initializing
+	static void Init() {_did_init = true;}
+	static bool _did_init;
 
-Stamp operator+(const Stamp &a, unsigned long msec);
-Stamp operator-(const Stamp &a, unsigned long msec);
+ public:
+	Stamp() {Init();}
+	// Don't need to call Init() in copy constructor, it must
+	// have been called when the value we're copying from was
+	// constructed
+
+	friend bool operator<(const Stamp &a, const Stamp &b);
+
+	friend Stamp operator+(const Stamp &a, unsigned long msec);
+	friend Stamp operator-(const Stamp &a, unsigned long msec);
 	
-long operator-(const Stamp &a, const Stamp &b);	
+	friend long operator-(const Stamp &a, const Stamp &b);	
 	
-void Init();	
+	friend Stamp getCurrentStamp();
+};
 	
-Stamp getCurrentStamp();
-	
-} // of namespace Time	
+inline Stamp operator+(unsigned long msec, const Stamp &a) {return a + msec;}
+
+} // of namespace Time
 
 #endif
