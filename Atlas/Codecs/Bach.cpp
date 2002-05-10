3fdde0c6983888efb06c7c486f57fdfc467da1d3
Michael Koch <konqueror@gmx.de>
2002-05-10 12:16:09 +0000

    New Bach Codec. Parser isnt working yet but writing the codec works.

---
+++ Atlas/Codecs/Bach.cpp
@@ -0,0 +1,569 @@
+// This file may be redistributed and modified under the terms of the
+// GNU Lesser General Public License (See COPYING for details).
+// Copyright (C) 2002 Michael Koch <konqueror@gmx.de>
+
+#include "Bach.h"
+
+namespace Atlas { namespace Codecs {
+
+Bach::Bach(std::iostream& s, Atlas::Bridge* b)
+    : m_socket(s)
+    , m_bridge(b)
+    , m_comma( false )
+{
+    m_state.push(PARSE_STREAM);
+}
+
+void Bach::parseStream(char next)
+{
+    cout << "parseStream" << endl;
+    switch (next)
+    {
+    case '{':
+        m_bridge->streamMessage(m_mapBegin);
+        m_state.push(PARSE_MAP);
+        m_state.push(PARSE_MAP_BEGIN);
+        break;
+/*
+    default:
+        cout << "unexpected character : " << next << endl;
+        // FIXME signal error here
+        // unexpected character
+        break;
+*/
+    }
+}
+
+void Bach::parseMap(char next)
+{
+    cout << "parseMap" << endl;
+
+    switch (next)
+    {
+    case '}':
+        m_bridge->mapEnd();
+        m_state.pop();
+        break;
+
+/*
+    case '{':
+        m_state.push(PARSE_MAP);
+        m_state.push(PARSE_MAP_BEGIN);
+        m_state.push(PARSE_DATA);
+        m_state.push(PARSE_NAME);
+        break;
+*/
+
+    case '[':
+        m_state.push(PARSE_LIST);
+        m_state.push(PARSE_LIST_BEGIN);
+        m_state.push(PARSE_DATA);
+        m_state.push(PARSE_NAME);
+        break;
+
+/*
+    case '\"':
+        m_state.push(PARSE_STRING);
+        m_state.push(PARSE_NAME);
+        break;
+
+    case '#':
+ 	m_state.push(PARSE_INT);
+ 	m_state.push(PARSE_NAME);
+        break;
+
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+    case '0':
+    case '-':
+    case '+':
+    case '.':
+        m_state.push(PARSE_FLOAT);
+        m_state.push(PARSE_NAME);
+        break;
+
+*/
+    case ',':
+        m_state.push(PARSE_DATA);
+        m_state.push(PARSE_NAME);
+        break;
+
+    default:
+        if (((next>='a')&&(next<='z'))||
+            ((next>='A')&&(next<='Z')))
+        {
+            m_socket.putback(next);
+            m_state.push(PARSE_DATA);
+            m_state.push(PARSE_NAME);
+        }
+        else
+        {
+            cout << "parseMap: unexpected character: " << next << endl;
+            // FIXME signal error here
+            // unexpected character
+        }
+        break;
+    }
+}
+
+void Bach::parseList(char next)
+{
+    cout << "parseList" << endl;
+
+    switch (next)
+    {
+    case ']':
+        m_bridge->listEnd();
+        m_state.pop();
+        break;
+
+    case '{':
+        m_bridge->listItem(m_mapBegin);
+        m_state.push(PARSE_MAP);
+        m_state.push(PARSE_MAP_BEGIN);
+        m_state.push(PARSE_NAME);
+        m_state.push(PARSE_DATA);
+        break;
+
+/*
+    case '\"':
+        m_state.push(PARSE_STRING);
+        break;
+
+    case ',':
+        m_state.pop();
+        break;
+
+    case '@':
+	m_state.push(PARSE_INT);
+        break;
+*/
+
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+    case '0':
+    case '-':
+        m_state.push(PARSE_FLOAT);
+        m_socket.putback(next);
+        break;
+
+/*
+    default:
+        // FIXME signal error here
+        // unexpected character
+        break;
+*/
+    }
+}
+
+void Bach::parseMapBegin(char next)
+{
+    cout << "parseMapBegin" << endl;
+
+    m_bridge->mapItem(decodeString(m_name), m_mapBegin);
+    m_socket.putback(next);
+    m_state.pop();
+    m_name.erase();
+}
+
+void Bach::parseListBegin(char next)
+{
+    cout << "parseListBegin" << endl;
+
+    m_bridge->mapItem(decodeString(m_name), m_listBegin);
+    m_socket.putback(next);
+    m_state.pop();
+    m_name.erase();
+}
+
+void Bach::parseInt(char next)
+{
+    cout << "parseInt" << endl;
+}
+
+void Bach::parseFloat(char next)
+{
+    cout << "parseFloat" << endl;
+
+    switch (next)
+    {
+    case '[':
+    case ']':
+    case '(':
+    case ')':
+    case ',':
+        m_socket.putback(next);
+        m_state.pop();
+        if (m_state.top() == PARSE_MAP)
+        {
+            cout << "Float: " << m_data << endl;
+
+            m_bridge->mapItem(decodeString(m_name), atof(m_data.c_str()));
+            m_name.erase();
+        }
+        else if (m_state.top() == PARSE_LIST)
+        {
+            cout << "Float: " << m_data << endl;
+
+            m_bridge->listItem(atof(m_data.c_str()));
+        }
+        else
+        {
+            cout << "Bach::parseFloat: Error" << endl;
+            // FIXME some kind of sanity checking assertion here
+        }
+        m_data.erase();
+	break;
+
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+    case '.':
+    case '-':
+    case '+':
+    case 'e':
+    case 'E':
+        m_data += next;
+	break;
+
+    default:
+        cout << "unexpected character: " << next << endl;
+        // FIXME signal error here
+        // unexpected character
+	break;
+    }
+}
+
+void Bach::parseString(char next)
+{
+    cout << "parseString" << endl;
+
+    switch (next)
+    {
+/*
+    case '[':
+    case ']':
+    case '(':
+    case ')':
+    case ',':
+        m_socket.putback(next);
+*/
+
+    case '\"':
+        m_state.pop();
+        if (m_state.top() == PARSE_MAP)
+        {
+            cout << "String: " << m_data << endl;
+
+            m_bridge->mapItem(decodeString(m_name), decodeString(m_data));
+            m_name.erase();
+        }
+        else if (m_state.top() == PARSE_LIST)
+        {
+            cout << "String: " << m_data << endl;
+
+            m_bridge->listItem(decodeString(m_data));
+        }
+        else
+        {
+            // FIXME some kind of sanity checking assertion here
+        }
+        m_data.erase();
+        break;
+
+/*
+    case '=':
+        // FIXME signal error here
+        // unexpected character
+        break;
+
+    case '\\':
+        // FIXME escape signs
+        break;
+*/
+
+    default:
+        m_data += next;
+        break;
+    }
+}
+
+void Bach::parseData(char next)
+{
+    cout << "parseData" << endl;
+
+    switch (next)
+    {
+    case '-':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+    case '0':
+        m_socket.putback(next);
+        m_state.pop();
+        m_state.push(PARSE_FLOAT);
+        break;
+
+    case '{':
+        m_state.pop();
+        m_state.push(PARSE_MAP);
+        m_state.push(PARSE_NAME);
+        m_state.push(PARSE_DATA);
+        break;
+
+    case '[':
+        m_state.pop();
+        m_state.push(PARSE_LIST);
+        m_state.push(PARSE_DATA);
+        break;
+
+    case '\"':
+        m_state.pop();
+        m_state.push(PARSE_STRING);
+        break;
+
+/*
+    default:
+        break;
+*/
+    }
+}
+
+void Bach::parseName(char next)
+{
+    cout << "parseName" << endl;
+
+    switch (next)
+    {
+    case ':':
+        cout << "Name: " << m_name << endl;
+        m_state.pop();
+	break;
+
+/*
+    case '[':
+    case ']':
+    case '(':
+    case ')':
+    case ',':
+        // FIXME signal error here
+        // unexpected character
+	break;
+*/
+
+    default:
+        m_name += next;
+	break;
+    }
+}
+
+void Bach::poll(bool can_read)
+{
+    if (!can_read) return;
+    do
+    {
+	char next = (char) m_socket.get();
+
+        cout << "Character: " << next << "   ";
+
+        switch (m_state.top())
+	{
+        case PARSE_STREAM:     parseStream(next); break;
+        case PARSE_MAP:        parseMap(next); break;
+        case PARSE_LIST:       parseList(next); break;
+        case PARSE_MAP_BEGIN:  parseMapBegin(next); break;
+        case PARSE_LIST_BEGIN: parseListBegin(next); break;
+        case PARSE_DATA:       parseData(next); break;
+        case PARSE_INT:	       parseInt(next); break;
+        case PARSE_FLOAT:      parseFloat(next); break;
+        case PARSE_STRING:     parseString(next); break;
+        case PARSE_NAME:       parseName(next); break;
+	}
+    }
+    while (m_socket.rdbuf()->in_avail());
+}
+
+const std::string Bach::decodeString(std::string toDecode)
+{
+    unsigned int pos;
+
+//    while((pos = toDecode.find( "\\\"" )) >= 0)
+//          toDecode.replace(pos, 2, "\"");
+
+//    while((pos = toDecode.find( "\\\\")) >= 0)
+//          toDecode.replace(pos, 2, "\\");
+
+    return toDecode;
+}
+
+const std::string Bach::encodeString(std::string toEncode)
+{
+    int pos;
+
+    while ((pos = toEncode.find ( '\\' )) >= 0)
+	toEncode.replace( pos, 1, "\\\\" );
+
+    while ((pos = toEncode.find ( '\"' )) >= 0)
+	toEncode.replace( pos, 1, "\\\"" );
+
+    return toEncode;
+}
+
+void Bach::writeItem(std::string name, long data)
+{
+    if( m_comma )
+	m_socket << ",";
+
+    if( name != "" )
+	m_socket << name << ":";
+
+    m_socket << data;
+}
+
+void Bach::writeItem(std::string name, double data)
+{
+    if( m_comma )
+	m_socket << ",";
+
+    if( name != "" )
+	m_socket << name << ":";
+
+    m_socket << data;
+}
+
+void Bach::writeItem(std::string name, std::string data)
+{
+    if( m_comma )
+	m_socket << ",";
+
+    if( name != "" )
+	m_socket << name << ":";
+
+    m_socket << "\"" << encodeString( data ) << "\"";
+}
+
+void Bach::writeLine(std::string line, bool endline, bool endtag)
+{
+    if (m_comma && !endtag)
+	m_socket << ",";
+
+    m_socket << line;
+}
+
+void Bach::streamBegin()
+{
+    writeLine( "[" );
+    m_comma = false;
+}
+
+void Bach::streamEnd()
+{
+    writeLine( "]", true, true );
+}
+
+void Bach::streamMessage(const Map&)
+{
+    writeLine( "{" );
+    m_comma = false;
+}
+
+void Bach::mapItem(const std::string& name, const Map&)
+{
+    writeLine( name + ":{" );
+    m_comma = false;
+}
+
+void Bach::mapItem(const std::string& name, const List&)
+{
+    writeLine( name + ":[" );
+    m_comma = false;
+}
+
+void Bach::mapItem(const std::string& name, long data)
+{
+    writeItem( name, data );
+    m_comma = true;
+}
+
+void Bach::mapItem(const std::string& name, double data)
+{
+    writeItem( name, data );
+    m_comma = true;
+}
+
+void Bach::mapItem(const std::string& name, const std::string& data)
+{
+    writeItem( name, data );
+    m_comma = true;
+}
+
+void Bach::mapEnd()
+{
+    writeLine( "}", true, true );
+    m_comma = true;
+}
+
+void Bach::listItem(const Map&)
+{
+    writeLine( "{" );
+    m_comma = false;
+}
+
+void Bach::listItem(const List&)
+{
+    writeLine( "[" );
+    m_comma = false;
+}
+
+void Bach::listItem(long data)
+{
+    writeItem( "", data );
+    m_comma = true;
+}
+
+void Bach::listItem(double data)
+{
+    writeItem( "", data );
+    m_comma = true;
+}
+
+void Bach::listItem(const std::string& data)
+{
+    writeItem( "", data );
+    m_comma = true;
+}
+
+void Bach::listEnd()
+{
+    writeLine( "]", true, true );
+    m_comma = true;
+}
+
+} } //namespace Atlas::Codecs
---
+++ Atlas/Codecs/Bach.h
@@ -0,0 +1,90 @@
+// This file may be redistributed and modified under the terms of the
+// GNU Lesser General Public License (See COPYING for details).
+// Copyright (C) 2000-2001 Michael Day, Stefanus Du Toit
+
+#ifndef ATLAS_CODECS_BACH_H
+#define ATLAS_CODECS_BACH_H
+
+#include <iostream>
+#include <stack>
+
+#include "../Codec.h"
+
+namespace Atlas { namespace Codecs {
+
+/** @file Codecs/Bach.h
+ * This class implements Bach Codec
+ */
+
+class Bach : public Codec<std::iostream>
+{
+  public:
+
+    Bach(std::iostream& s, Atlas::Bridge* b);
+
+    virtual void poll(bool can_read = true);
+
+    virtual void streamBegin();
+    virtual void streamMessage(const Map&);
+    virtual void streamEnd();
+
+    virtual void mapItem(const std::string& name, const Map&);
+    virtual void mapItem(const std::string& name, const List&);
+    virtual void mapItem(const std::string& name, long);
+    virtual void mapItem(const std::string& name, double);
+    virtual void mapItem(const std::string& name, const std::string&);
+    virtual void mapEnd();
+
+    virtual void listItem(const Map&);
+    virtual void listItem(const List&);
+    virtual void listItem(double);
+    virtual void listItem(long);
+    virtual void listItem(const std::string&);
+    virtual void listEnd();
+
+  protected:
+
+    std::iostream& m_socket;
+    Bridge* m_bridge;
+    bool m_comma;
+
+    enum State
+    {
+	PARSE_STREAM,
+        PARSE_MAP,
+        PARSE_LIST,
+	PARSE_MAP_BEGIN,
+	PARSE_LIST_BEGIN,
+        PARSE_NAME,
+        PARSE_DATA,
+        PARSE_INT,
+        PARSE_FLOAT,
+        PARSE_STRING
+    };
+
+    std::string m_name, m_data;
+    std::stack<State> m_state;
+
+    inline void parseStream(char);
+    inline void parseMap(char);
+    inline void parseList(char);
+    inline void parseMapBegin(char);
+    inline void parseListBegin(char);
+    inline void parseData(char);
+    inline void parseInt(char);
+    inline void parseFloat(char);
+    inline void parseString(char);
+    inline void parseName(char);
+
+    inline const std::string encodeString(std::string);
+    inline const std::string decodeString(std::string);
+
+    void writeItem(std::string,long);
+    void writeItem(std::string,double);
+    void writeItem(std::string,std::string);
+    void writeLine(std::string,bool=true,bool=false);
+};
+
+} } // namespace Atlas::Codecs
+
+#endif // ATLAS_CODECS_BACH_H
