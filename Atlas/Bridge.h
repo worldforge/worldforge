de6894642565cc77dfeda45bb75ef182108745a4
Michael Koch <konqueror@gmx.de>
2002-05-15 14:14:40 +0000

    2002-05-15  Michael Koch  <konqueror@gmx.de>
            * src/Bridge.h: added documentation.

--- Atlas/Bridge.h
+++ Atlas/Bridge.h
@@ -34,26 +34,71 @@ class Bridge
       
     // Interface for stream context
 
+    /**
+     *  Begin an Atlas stream.
+     */
     virtual void streamBegin() = 0;
+    /**
+     *  Start a message in an Atlas stream.
+     */
     virtual void streamMessage(const Map&) = 0;
+    /**
+     *  Ends the Atlas stream.
+     */
     virtual void streamEnd() = 0;
     
     // Interface for map context
 
+    /**
+     *  Starts a map object to the currently streamed map.
+     */
     virtual void mapItem(const std::string& name, const Map&) = 0;
+    /**
+     *  Starts a list object to the currently streamed map.
+     */
     virtual void mapItem(const std::string& name, const List&) = 0;
+    /**
+     *  Adds an integer to the currently streames map.
+     */
     virtual void mapItem(const std::string& name, long) = 0;
+    /**
+     *  Adds a float to the currently streamed map.
+     */
     virtual void mapItem(const std::string& name, double) = 0;
+    /**
+     *  Adds a string to the currently streamed map.
+     */
     virtual void mapItem(const std::string& name, const std::string&) = 0;
+    /**
+     *  Ends the currently streamed map.
+     */
     virtual void mapEnd() = 0;
     
     // Interface for list context
     
+    /**
+     *  Starts a map object in the currently streamed list.
+     */
     virtual void listItem(const Map&) = 0;
+    /**
+     *  Starts a list object in the currently streamed list.
+     */
     virtual void listItem(const List&) = 0;
+    /**
+     *  Adds an integer to the currently streames list.
+     */
     virtual void listItem(long) = 0;
+    /**
+     *  Adds a float to the currently streamed list.
+     */
     virtual void listItem(double) = 0;
+    /**
+     *  Adds a string to the currently streamed list.
+     */
     virtual void listItem(const std::string&) = 0;
+    /**
+     *  Ends the currently streamed list.
+     */
     virtual void listEnd() = 0;
 };
 
