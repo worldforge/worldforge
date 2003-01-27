2afda91da9aaf4827231b38855a9cb563616f0e5
Al Riddoch <alriddoch@googlemail.com>
2003-01-27 23:40:07 +0000

    Work in progress

--- Mercator/Segment.cpp
+++ Mercator/Segment.cpp
@@ -14,6 +14,7 @@ Segment::Segment(int res) : m_res(res), m_points(new float[res * res])
 
 void Segment::populate(float nn, float fn, float ff, float nf)
 {
+#if 1
     // A somewhat wacky proof of concept interpolation
     for(int i = 0; i < m_res; ++i) {
         float near = nn + ((float)i / m_res) * (fn - nn);
@@ -22,6 +23,41 @@ void Segment::populate(float nn, float fn, float ff, float nf)
             m_points[j * m_res + i] = near + ((float)j / m_res) * (far - near);
         }
     }
+#else
+    float tmp[3 * m_res + 1][3 * m_res +1];
+    tmp[0][0] = nn;
+    tmp[0][m_res] = nn;
+    tmp[m_res][0] = nn;
+    tmp[m_res][m_res] = nn;
+
+    tmp[m_res * 2][m_res] = fn;
+    tmp[m_res * 3][m_res] = fn;
+    tmp[m_res * 2][0] = fn;
+    tmp[m_res * 3][0] = fn;
+
+    tmp[m_res][m_res * 2] = nf;
+    tmp[m_res][m_res * 3] = nf;
+    tmp[0][m_res * 2] = nf;
+    tmp[0][m_res * 3] = nf;
+
+    tmp[m_res * 2][m_res * 2] = ff;
+    tmp[m_res * 3][m_res * 2] = ff;
+    tmp[m_res * 2][m_res * 3] = ff;
+    tmp[m_res * 3][m_res * 3] = ff;
+
+    for(int l = m_res, m = 1; l > 0; l >>= 1, m <<= 1) {
+        for(int 0 = 1; i < (m * 3); i++) {
+            for(int 0 = 1; j < (m * 3); j++) {
+                float tot = tmp[j * l][i * l] +
+                            tmp[j * l][(i + 1) * l] +
+                            tmp[(j + 1) * l][i * l] +
+                            tmp[(j + 1) * l][(i + 1) * l];
+                tmp[j * l + l / 2][i * l + l / 2] = tot / 4;
+            }
+        }
+    }
+
+#endif
 }
 
 } // namespace Mercator
