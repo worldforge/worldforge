41ccb7d94cd23b9922e1d76e32c71e4cdd54d9de
Al Riddoch <alriddoch@googlemail.com>
2003-07-27 21:59:32 +0000

    2003-07-27  Al Riddoch  <alriddoch@zepler.org>
    	* Mercator/DepthShader.cpp, Mercator/DepthShader.h:
    	  Add a shader for tinting terrain below water level to simulate
    	  light attenuation due to water.

---
+++ Mercator/DepthShader.cpp
@@ -0,0 +1,52 @@
+// This file may be redistributed and modified only under the terms of
+// the GNU General Public License (See COPYING for details).
+// Copyright (C) 2003 Alistair Riddoch
+
+#include <Mercator/DepthShader.h>
+
+#include <Mercator/Segment.h>
+#include <Mercator/Surface.h>
+
+namespace Mercator {
+
+DepthShader::DepthShader(float waterLevel, float murkyDepth) : Shader(true, true),
+              m_waterLevel(waterLevel), m_murkyDepth(murkyDepth)
+{
+}
+
+DepthShader::~DepthShader()
+{
+}
+
+void DepthShader::shade(Surface & s) const
+{
+    unsigned int channels = s.getChannels();
+    assert(channels > 0);
+    unsigned int colors = channels - 1;
+    float * data = s.getData();
+    const float * height_data = s.getSegment().getPoints();
+    if (height_data == 0) {
+        std::cerr << "WARNING: Mercator: Attempting to shade empty segment."
+                  << std::endl << std::flush;
+        return;
+    }
+    unsigned int size = s.getSegment().getSize();
+
+    unsigned int count = size * size;
+    int j = -1;
+    for (unsigned int i = 0; i < count; ++i) {
+        for (unsigned int k = 0; k < colors; ++k) {
+            data[++j] = 1.f;
+        }
+        float depth = height_data[i];
+        if (depth > m_waterLevel) {
+            data[i] = 0.f;
+        } else if (depth < m_murkyDepth) {
+            data[i] = 1.f;
+        } else {
+            data[i] = (depth - m_murkyDepth) / (m_waterLevel - m_murkyDepth);
+        }
+    }
+}
+
+} // namespace Mercator
---
+++ Mercator/DepthShader.h
@@ -0,0 +1,25 @@
+// This file may be redistributed and modified only under the terms of
+// the GNU General Public License (See COPYING for details).
+// Copyright (C) 2003 Alistair Riddoch
+
+#ifndef MERCATOR_FILL_DEPTH_SHADER_H
+#define MERCATOR_FILL_DEPTH_SHADER_H
+
+#include <Mercator/Shader.h>
+
+namespace Mercator {
+
+class DepthShader : public Shader {
+  private:
+    float m_waterLevel;
+    float m_murkyDepth;
+  public:
+    explicit DepthShader(float waterLevel = 0.f, float murkyDepth = 64.f);
+    virtual ~DepthShader();
+
+    virtual void shade(Surface &) const;
+};
+
+} // namespace Mercator
+
+#endif // MERCATOR_FILL_DEPTH_SHADER_H
