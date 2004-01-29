6a5cca6baf34488696beac5cc0c2b3caf136b337
James Turner <james@worldforge.org>
2004-01-29 16:17:17 +0000

    Thursday, January 29th, James Turner <james@worldforge.org>
            * Add a test for the timestamp/timediff code (not exhaustive
              by any means)

---
+++ wfmath/timestamp_test.cpp
@@ -0,0 +1,51 @@
+
+#include <wfmath/timestamp.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace WFMath;
+using std::cout;
+using std::endl;
+
+int main()
+{
+
+    TimeStamp tsa;
+    if (tsa.isValid()) {
+        cout << "default constructor returned valid" << endl;
+        return EXIT_FAILURE;
+    }
+    
+    tsa += TimeDiff(50);
+    if (tsa.isValid()) {
+        cout << "invalid stamp += valid time diff returned valid" << endl;
+        return EXIT_FAILURE;
+    }
+    
+    tsa = TimeStamp::now();
+    if (!tsa.isValid()) return EXIT_FAILURE;
+    
+    tsa += TimeDiff();
+    if (tsa.isValid()) {
+        cout << "valid time += invalid diff returned valid" << endl;
+        return EXIT_FAILURE;
+    }
+    
+    TimeDiff tda(1000);
+    if (!tda.isValid()) {
+        cout << "construct diff from literal msecs is broken" << endl;
+        return EXIT_FAILURE;
+    }
+    
+    tsa = TimeStamp::now();
+    TimeStamp tsb = tsa;
+    if (tsa != tsb) {
+        cout << "equality of stamps is broken" << endl;
+        return EXIT_FAILURE; // equality is broken
+    }
+    
+    tsb += tda;
+    if (tsa >= tsb) return EXIT_FAILURE; // compare is broken
+    
+    return EXIT_SUCCESS;
+}
