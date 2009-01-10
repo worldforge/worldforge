7c304be55637852416b9ced216fc0ce5ab7b7345
Al Riddoch <alriddoch@googlemail.com>
2009-01-10 16:51:32 +0000

    Add some more specialisations that code uses

--- wfmath/point.cpp
+++ wfmath/point.cpp
@@ -189,4 +189,7 @@ template Point<2> Barycenter<2, std::vector, std::list>(const std::vector<Point<
 template Point<3> operator-<3>(const Point<3> &, const Vector<3> &);
 template Point<2> operator-<2>(const Point<2> &, const Vector<2> &);
 
+template Point<3> operator+<3>(const Vector<3> &, const Point<3> &);
+template Point<2> operator+<2>(const Vector<2> &, const Point<2> &);
+
 } // namespace WFMath
