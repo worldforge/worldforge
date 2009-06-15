e0853b894686f018d283d8a60a2ad95c85786162
Alistair Riddoch <alriddoch@googlemail.com>
2009-06-15 02:22:21 +0100

    Add coverage test for BasePoint

---
+++ tests/testBasePoint.cpp
@@ -0,0 +1,12 @@
+// This file may be redistributed and modified only under the terms of
+// the GNU General Public License (See COPYING for details).
+// Copyright (C) 2009 Alistair Riddoch
+
+#include <Mercator/BasePoint.h>
+
+int main()
+{
+    Mercator::BasePoint bp1;
+
+    return 0;
+}
