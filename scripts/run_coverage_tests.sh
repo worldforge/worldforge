fff535848073fbff3c6e9fd7f4091cf8c8007947
Alistair Riddoch <alriddoch@googlemail.com>
2009-07-23 18:54:50 +0100

    Update the coverage test script to get the features added to cyphesis.

--- scripts/run_coverage_tests.sh
+++ scripts/run_coverage_tests.sh
@@ -4,12 +4,12 @@ check_coverage() {
     source_file=$1
     source_dir=$(dirname ${source_file})
     base_file=$(basename ${source_file} .cpp)
-    test_program=tests/test${base_file}
+    test_program=tests/${base_file}test
     coverage_data=${source_dir}/${base_file}.gcda
 
     if [ ! -f ${test_program}.cpp ]
     then
-        if [ ${report} -eq 1 ]
+        if [ ${report} -eq 1 -o ${single} -eq 1 ]
         then
             echo No test for ${source_file}
         fi
@@ -49,6 +49,10 @@ check_coverage() {
     then
         if [ ! -f ${source_dir}/${base_file}.gcno ]
         then
+            if [ ${single} -eq 1 ]
+            then
+                echo No code to be covered in ${source_file}
+            fi
             return
         fi
         echo No coverage data ${coverage_data} for ${source_file}
@@ -62,7 +66,7 @@ check_coverage() {
     fi
 
 
-    coverage_percent=$( (cd ${source_dir} && gcov ${source_file}) | \
+    coverage_percent=$( (cd ${source_dir} && LC_ALL=C gcov ${source_file}) | \
           grep -A 1 "^File '$(basename ${source_file})" | \
           grep ^Lines | \
           head -n 1 | \
@@ -72,16 +76,45 @@ check_coverage() {
 }
 
 usage() {
-    echo Automate running coverage tools, and generate concise report.
-    echo usage: run_coverage_tests.sh [ -r ] [SOURCEFILE]
-    echo Options:
-    echo "  -r Just report source files with a coverage test"
+    echo "Automate running coverage tools, and generate concise report."
+    echo "usage: run_coverage_tests.sh [OPTION]"
+    echo "       run_coverage_tests.sh [SOURCEFILE]"
+    echo "Options:"
+    echo "  -r Report source files with a coverage test"
+    echo "  -c Re-configure the build to include coverage data"
+    echo "  -h Display this usage data"
+    cat <<EOF
+
+To pruduce results run this build requires an in-tree build with gcc
+configured to include coverage data. Use the -c option to run configure
+with the right options, and run "make clean all check" to fully prepare
+the build. Running without arguments shows results for all source files
+in the configured directories.
+
+Each file is covered by a test derived from it's name. For example the
+following file:
+
+rulesets/Character.cpp
+
+should be covered by;
+
+tests/Charactertest.cpp
+
+To see line by line data, run this script on Character.cpp only:
+
+./scripts/run_coverage_tests.sh rulesets/Character.cpp
+
+and the line by line report will be in this file:
+
+rulesets/Character.cpp.gcov
+EOF
 }
 
 DIRS="Mercator"
 
 declare -i report=0
 declare -i configure=0
+declare -i single=0
 
 while getopts "hrc" options
 do
@@ -97,7 +130,7 @@ done
 
 if [ ${configure} -eq 1 ]
 then
-    CXXFLAGS="-g --coverage" ./configure --enable-debug=yes --enable-static --disable-shared
+    CXXFLAGS="-g --coverage" ./configure --prefix=/opt/worldforge --enable-debug=yes --enable-binreloc=no
     exit 0
 fi
 
@@ -107,6 +140,7 @@ if [ -n "$1" ]
 then
     if [ -f "$1" ]
     then
+        single=1
         check_coverage $1
         exit 0
     else
