60c6ec0160cc72a9a52a3a6f16c2068eb2fda03d
Alistair Riddoch <alriddoch@googlemail.com>
2009-07-23 17:48:12 +0100

    Add test for Matrix class.

---
+++ tests/testMatrix.cpp
@@ -0,0 +1,15 @@
+// This file may be redistributed and modified only under the terms of
+// the GNU General Public License (See COPYING for details).
+// Copyright (C) 2009 Alistair Riddoch
+
+#include <Mercator/Matrix.h>
+
+int main()
+{
+    Mercator::Matrix<2,2> m;
+    Mercator::Matrix<4,4> n;
+
+    m(0,0) = 1.f;
+
+    return 0;
+}
