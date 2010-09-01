91641e57cf20f411f91001a2fff68e077301b38a
Alistair Riddoch <alriddoch@googlemail.com>
2010-09-01 20:06:51 +0100

    Add a proof of concept isolated unit test for Entity, not yet complete

--- test/Entity_unittest.cpp
+++ test/Entity_unittest.cpp
@@ -17,7 +17,90 @@
 
 // $Id$
 
+#include <Eris/Entity.h>
+
+#include <Eris/Alarm.h>
+#include <Eris/Exceptions.h>
+#include <Eris/Log.h>
+#include <Eris/Task.h>
+#include <Eris/TypeInfo.h>
+
+class TestErisEntity : public Eris::Entity
+{
+  public:
+    TestErisEntity(const std::string & id, Eris::TypeInfo * tp) : Eris::Entity(id, tp) { }
+
+    virtual Eris::TypeService* getTypeService() const { return 0; }
+    virtual void removeFromMovementPrediction() { }
+    virtual void addToMovementPredition() { }
+    virtual Eris::Entity* getEntity(const std::string&) { return 0; }
+
+};
+
 int main()
 {
+    {
+        Eris::Entity * e = new TestErisEntity("1", 0);
+        e->shutdown();
+        delete e;
+    }
+
+
     return 0;
+}
+
+// stubs
+
+namespace Eris {
+
+Alarm::Alarm(unsigned long msec, const sigc::slot<void>& done)
+{
+}
+
+void Alarm::expired()
+{
+}
+
+const Atlas::Message::Element* TypeInfo::getAttribute(const std::string& attributeName) const
+{
+    return 0;
+}
+
+TypeInfoPtr TypeService::getTypeByName(const std::string &id)
+{
+    return 0;
+}
+
+Task::Task(Entity* owner, const std::string& nm) :
+    m_name(nm),
+    m_owner(owner),
+    m_progress(0.0),
+    m_progressRate(-1.0)
+{
+}
+
+Task::~Task()
+{
+}
+
+void Task::updateFromAtlas(const Atlas::Message::MapType & d)
+{
+}
+
+BaseException::~BaseException() throw()
+{
+}
+
+InvalidOperation::~InvalidOperation() throw()
+{
+}
+
+void doLog(LogLevel lvl, const std::string& msg)
+{
+}
+
+void mergeOrCopyElement(const Atlas::Message::Element& src, Atlas::Message::Element& dst)
+{
+}
+
 }
