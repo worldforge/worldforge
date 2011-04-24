e8e88210af24f41a15ac382e910a03239abffa74
Alistair Riddoch <alriddoch@googlemail.com>
2011-04-24 21:23:15 +0100

    Use macro to get efficient sqrt()

--- wfmath/vector.cpp
+++ wfmath/vector.cpp
@@ -97,7 +97,7 @@ Vector<3>& WFMath::_NCFS_Vector3_rotate(Vector<3>& v, const Vector<3>& axis,
   assert(axis_sqr_mag != 0);
 
   Vector<3> perp_part = v - axis * Dot(v, axis) / axis_sqr_mag;
-  Vector<3> rot90 = Cross(axis, perp_part) / (CoordType) sqrt(axis_sqr_mag);
+  Vector<3> rot90 = Cross(axis, perp_part) / F_SQRT(axis_sqr_mag);
 
   v += perp_part * (F_COS(theta) - 1) + rot90 * F_SIN(theta);
 
