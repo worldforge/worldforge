e38b70c23d2a28f26e203e837dbdfc5bf27ce0ec
Sean Ryan <sryan@evercrack.com>
2011-05-09 00:44:37 -0400

    Figured out the packet type decode

--- src/MetaServer.hpp
+++ src/MetaServer.hpp
@@ -1,5 +1,6 @@
 #include <ctime>
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <map>
 #include <boost/array.hpp>
@@ -7,12 +8,14 @@
 #include <boost/shared_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
 #include <boost/asio.hpp>
+#include <boost/foreach.hpp>
 
 #ifndef __METASERVER_HPP__
 
 #define __METASERVER_HPP__
 
 #define METASERVER_DEFAULT_PORT 12345
+#define foreach         		BOOST_FOREACH
 
 using boost::asio::ip::tcp;
 using boost::asio::ip::udp;
--- src/MetaServerHandlerUDP.cpp
+++ src/MetaServerHandlerUDP.cpp
@@ -32,17 +32,32 @@ MetaServerHandlerUDP::start_receive()
 
 void
 MetaServerHandlerUDP::handle_receive(const boost::system::error_code& error,
-									 std::size_t bytes_transferred)
+									 std::size_t bytes_recvd)
 {
 	std::cout << "handle_receive" << std::endl;
 	if(!error || error == boost::asio::error::message_size )
 	{
-		std::cout << "UDP-1 : read off packet [" << bytes_transferred << "]" << std::endl;
-		std::cout << "UDP-2 : analyse packet" << std::endl;
-		std::cout << "      : bytes [ " << bytes_transferred << " ]" << std::endl;
-		std::cout << "      : from  [ " << remote_endpoint_.address().to_string() << " ]" << std::endl;
-		std::cout << "      : buffer[ " << recv_buffer_ << " ]" << std::endl;
 
+		std::cout << "UDP-1 : read off packet [" << bytes_recvd << "]" << std::endl;
+		std::cout << "UDP-2 : analyse packet" << std::endl;
+		std::cout << "      : bytes [ " << bytes_recvd << " ]" << std::endl;
+		std::cout << "      : from  [ " << remote_endpoint_.address().to_string() << " ]" << std::endl;
+		std::cout << "      : sizeof int " << sizeof(int) << std::endl;
+
+		/**
+		 *  First 4 bytes are the packet type aka a int
+		 */
+		char rawTypeBytes[4];
+		     rawTypeBytes[0] = recv_buffer_.at(0);
+		     rawTypeBytes[1] = recv_buffer_.at(1);
+		     rawTypeBytes[2] = recv_buffer_.at(2);
+		     rawTypeBytes[3] = recv_buffer_.at(3);
+
+		int rawType = 0;
+		     memcpy( (void *)&rawType, rawTypeBytes , sizeof(rawType) );
+		int packetType = htonl(rawType);
+
+		std::cout << "      : packet type " << packetType << std::endl;
 		std::cout << "UDP-3 : make call to ms object" << std::endl;
 		std::cout << "UDP-4 : get response from ms" << std::endl;
 		std::cout << "UDP-5 : construct client response packet" << std::endl;
