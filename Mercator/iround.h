0fc27fa763bf60e42cbf2484de8d4bff456899aa
Alistair Riddoch <alriddoch@googlemail.com>
2012-02-08 22:30:02 +0000

    Remove now obsolete F_ macros

--- Mercator/iround.h
+++ Mercator/iround.h
@@ -9,6 +9,9 @@
 #error iround.h must be included after config.h
 #endif
 
+// FIXME tr1 contains std::tr1::lrint which is type overloaded
+// In c++0x this becomes std::lrint
+
 #ifdef HAVE_LRINTF
     #define I_ROUND(_x) (::lrintf(_x)) 
 #elif defined(HAVE_RINTF)
@@ -17,30 +20,6 @@
     #define I_ROUND(_x) ((int)::rint(_x)) 
 #else
     #define I_ROUND(_x) ((int)(_x)) 
-#endif
-
-#ifdef HAVE_FABSF
-    #define F_ABS(_x) (::fabsf(_x))
-#else
-    #define F_ABS(_x) (::fabs(_x))
-#endif
-
-#ifdef HAVE_POWF
-    #define F_POW(_x, _y) (::powf(_x, _y))
-#else
-    #define F_POW(_x, _y) (::pow(_x, _y))
-#endif
-
-#ifdef HAVE_FLOORF
-    #define F_FLOOR(_x) (::floorf(_x))
-#else
-    #define F_FLOOR(_x) (::floor(_x))
-#endif
-
-#ifdef HAVE_CEILF
-    #define F_CEIL(_x) (::ceilf(_x))
-#else
-    #define F_CEIL(_x) (::ceil(_x))
 #endif
 
 #endif // MERCATOR_IROUND_H
