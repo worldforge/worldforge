8d3c88932166bf4fee154a5e221257e07d593995
Alistair Riddoch <alriddoch@googlemail.com>
2012-02-22 14:47:50 +0000

    Exercise variable roughness

--- tests/Segmentperf.cpp
+++ tests/Segmentperf.cpp
@@ -20,6 +20,12 @@ int main(int argc, char ** argv)
 
     Mercator::Segment s(0,0,64);
 
+    Mercator::Matrix<2,2,Mercator::BasePoint> & points = s.getControlPoints();
+    points(0, 0).roughness() = 1.85;
+    points(1, 0).roughness() = 1.75;
+    points(0, 1).roughness() = 1.65;
+    points(1, 1).roughness() = 1.95;
+
     for (int i = 0; i < iterations; ++i) {
 
         s.populate();
