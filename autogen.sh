e9ab7ec1ac1a38f4a2d75139140efdcc9b9791eb
Erik Ogenvik <erik@ogenvik.org>
2013-04-21 13:30:18 +0200

    Harmonize autogen.sh within project.
    This version should work on all supported platforms, opting to using
    autoreconf if available.

--- autogen.sh
+++ autogen.sh
@@ -1,45 +1,55 @@
 #! /bin/sh
 
-echo aclocal...
-(aclocal --version) < /dev/null > /dev/null 2>&1 || {
-    echo aclocal not found
-    exit 1
-}
+rm -f config.cache
 
-aclocal -I m4
+#Check if the autoreconf command is available, and use that if so.
+if command -v autoreconf >/dev/null 2>&1 ; then
+  echo autoreconf...
+  autoreconf --install
+else
+  if test -d /usr/local/share/aclocal ; then
+    ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I /usr/local/share/aclocal"
+  fi
 
-echo autoheader...
-(autoheader --version) < /dev/null > /dev/null 2>&1 || {
-    echo autoheader not found
-    exit 1
-}    
+  (command -v aclocal) < /dev/null > /dev/null 2>&1 || {
+      echo aclocal not found
+      exit 1
+  }
+  echo aclocal...
+  aclocal -I m4 $ACLOCAL_FLAGS
 
-autoheader
+  #The GNU libtoolize is called 'glibtoolize' on Darwin.
+  if [ "`echo $OSTYPE | grep darwin`" != "" ] ; then
+    LIBTOOLIZE="glibtoolize"
+  else
+    LIBTOOLIZE="libtoolize"
+  fi
 
-echo libtoolize...
-(libtoolize --version) < /dev/null > /dev/null 2>&1 || {
-    echo libtoolize not found
-    exit 1
-}
+  (command -v $LIBTOOLIZE) < /dev/null > /dev/null 2>&1 || {
+      echo $LIBTOOLIZE not found
+      exit 1
+  }
+  echo $LIBTOOLIZE...
+  $LIBTOOLIZE --force --copy
 
-libtoolize --automake --copy --force
+  (command -v autoheader) < /dev/null > /dev/null 2>&1 || {
+      echo autoheader not found
+      exit 1
+  }
+  echo autoheader...
+  autoheader
 
-echo automake...
-(automake --version) < /dev/null > /dev/null 2>&1 || {
-    echo automake not found
-    exit 1
-}
+  (command -v automake) < /dev/null > /dev/null 2>&1 || {
+      echo automake not found
+      exit 1
+  }
+  echo automake...
+  automake --gnu --add-missing --copy
 
-automake --add-missing --copy --gnu
-
-echo autoconf...
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-    echo autoconf not found
-    exit 1
-}
-
-autoconf
-
-echo ready to configure
-
-exit 0
+  (command -v autoconf) < /dev/null > /dev/null 2>&1 || {
+      echo autoconf not found
+      exit 1
+  }
+  echo autoconf...
+  autoconf
+fi
