023a29fea60d999b67e633f227efd2cfbb284c9e
Peter Szücs <peter.szucs.dev@gmail.com>
2014-01-03 00:50:27 +0100

    Remove workaround.

--- Mercator/iround.h
+++ Mercator/iround.h
@@ -5,13 +5,7 @@
 #ifndef MERCATOR_IROUND_H
 #define MERCATOR_IROUND_H
 
-//MSVC 11.0 doesn't support std::lround so we'll use boost. When MSVC gains support for std::lround this could be removed.
-#ifdef _MSC_VER
-#include <boost/math/special_functions/round.hpp>
-#define I_ROUND(_x) (boost::math::lround(_x))
-#else
 #include <cmath>
 #define I_ROUND(_x) (std::lround(_x))
-#endif
 
 #endif // MERCATOR_IROUND_H
