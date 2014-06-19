5d243ab82fe80ac32ee77bd62496c7fef6912f08
Peter Szücs <peter.szucs.dev@gmail.com>
2014-06-19 12:25:14 +0200

    Fix Android.

--- Eris/iround.h
+++ Eris/iround.h
@@ -5,11 +5,11 @@
 #ifndef ERIS_IROUND_H
 #define ERIS_IROUND_H
 
-//MSVC 11.0 doesn't support std::lround so we'll use boost. When MSVC gains support for std::lround this could be removed.
-#ifdef _MSC_VER
-#include <boost/math/special_functions/round.hpp>
-#define I_ROUND(_x) (boost::math::lround(_x))
-#define L_ROUND(_x) (boost::math::llround(_x))
+// std::lround is missing on Android: https://code.google.com/p/android/issues/detail?id=54418
+#ifdef __ANDROID__
+#include <cmath>
+#define I_ROUND(_x) (lroundf(_x))
+#define L_ROUND(_x) (llround(_x))
 #else
 #include <cmath>
 #define I_ROUND(_x) (std::lround(_x))
