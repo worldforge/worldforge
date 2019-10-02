00a2f32faf15d953e9d25e1f19fe97052f966954
Erik Ogenvik <erik@ogenvik.org>
2019-10-02 14:20:32 +0200

    Run through ShellCheck.

--- tools/support/generate-ChangeLog.sh
+++ tools/support/generate-ChangeLog.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/bash
 #A script for combining the legacy ChangeLog found in ChangeLog-CVS with one output from git. The script will only run if the current ChangeLog contains exactly one line.
 #This makes it possible to do a new "make dist" from an existing source distribution (as the ChangeLog would then be complete.).
 top_srcdir=$1
@@ -10,11 +10,11 @@ if [[ x${distdir} = "x" ]] || [[ x${top_srcdir} = "x" ]] || [[ x${commitid} = "x
 	exit 1
 fi
 #Only do the aggregation if the ChangeLog file is exactly one line. If not the aggregation has already been done.
-if [[ `cat ${distdir}/ChangeLog | wc -l` = "0" ]]; then
+if [[ $(wc -l < "${distdir}/ChangeLog") = "0" ]]; then
 	echo "Generating ChangeLog by appending the old CVS ChangeLog to the one generated from git. This requires that you create the dist in the git repository."
 
-    cd ${top_srcdir}
-	chmod u+w ${distdir}/ChangeLog && git log ${commitid}..HEAD --stat --name-only --date=short --abbrev-commit > ${distdir}/ChangeLog && echo "" >> ${distdir}/ChangeLog && cat ${top_srcdir}/tools/support/ChangeLog-CVS >> ${distdir}/ChangeLog
+    cd "${top_srcdir}" || exit
+	chmod u+w "${distdir}/ChangeLog" && git log "${commitid}"..HEAD --stat --name-only --date=short --abbrev-commit > "${distdir}"/ChangeLog && echo "" >> "${distdir}/ChangeLog" && cat "${top_srcdir}/tools/support/ChangeLog-CVS" >> "${distdir}/ChangeLog"
 	#Put a notice in the legacy ChangeLog-CVS file, thus bringing the size of the dist down a bit.
-	chmod u+w ${distdir}/tools/support/ChangeLog-CVS && echo "This file was needed for generating the proper ChangeLog as an aggregate of the code held in git and older code in CVS. It's now empty, but needs to be included in the source distribution to not upset automake." > ${distdir}/tools/support/ChangeLog-CVS
+	chmod u+w "${distdir}/tools/support/ChangeLog-CVS" && echo "This file was needed for generating the proper ChangeLog as an aggregate of the code held in git and older code in CVS. It's now empty, but needs to be included in the source distribution to not upset automake." > "${distdir}/tools/support/ChangeLog-CVS"
 fi
