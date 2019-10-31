4425828ce932909cb7a6d79e126caf20e8bb7603
Erik Ogenvik <erik@ogenvik.org>
2019-10-31 21:39:30 +0100

    Removed findboost_modular.

--- cmake_findboost_modular/test_package/test_package.cpp
+++
@@ -1,9 +0,0 @@
-#include <boost/regex.hpp>
-#include <boost/date_time.hpp>
-
-int main()
-{
-    boost::gregorian::date weekstart(2002,boost::gregorian::Feb,1);
-    boost::regex expression{"\\w+\\s\\w+"};
-}
-
