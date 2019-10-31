9885b674ef7855aeb0962023b85f9d3c5ee9b12e
Erik Ogenvik <erik@ogenvik.org>
2019-10-31 15:51:17 +0100

    Fixes for win32.

--- lua/conanfile.py
+++ lua/conanfile.py
@@ -35,7 +35,7 @@ class Lua(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy('*lua.lib', dst='lib', keep_path=False)
+        self.copy('*.lib', dst='lib', keep_path=False)
         self.copy('*.dll', dst='lib', keep_path=False)
         self.copy('*.so', dst='lib', keep_path=False)
         self.copy('*.dylib', dst='lib', keep_path=False)
