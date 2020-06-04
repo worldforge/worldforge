ade9f59c6c7e7c7f1bb4b619727d8d3400ea4a9d
Erik Ogenvik <erik@ogenvik.org>
2020-06-05 00:40:03 +0200

    Build with PIC by default.

--- tools/conan/conanfile.py
+++ tools/conan/conanfile.py
@@ -11,8 +11,8 @@ class WfmathConan(ConanFile):
     description = "A math library for the Worldforge system."
     topics = ("mmorpg", "worldforge")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [False, True]}
-    default_options = {"shared": False}
+    options = {"shared": [False, True], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
     generators = "cmake"
 
     scm = {
