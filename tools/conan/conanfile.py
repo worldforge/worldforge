6df2008e45ef304a72bdbe513658897fcb4fd71c
Erik Ogenvik <erik@ogenvik.org>
2023-02-01 20:57:36 +0100

    Use CMake targets.

--- tools/conan/conanfile.py
+++ tools/conan/conanfile.py
@@ -12,7 +12,7 @@ class ErisConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [False, True], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
-    generators = "cmake"
+    generators = "cmake_find_package", "cmake_paths"
     requires = ["sigc++/2.10.0@worldforge/stable",
                 "atlas/0.7.0@worldforge/testing",
                 "wfmath/1.0.3@worldforge/testing",
@@ -35,7 +35,6 @@ class ErisConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.includedirs = ["include/eris-1.4"]
 
     def package(self):
         pass
