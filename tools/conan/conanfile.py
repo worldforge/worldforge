c924cc2c8a503301e897e8ae6ca47ecca184ecf0
Erik Ogenvik <erik@ogenvik.org>
2023-04-05 23:43:45 +0200

    Convert to Conan 2.

--- tools/conan/conanfile.py
+++ tools/conan/conanfile.py
@@ -1,4 +1,10 @@
-from conans import ConanFile, CMake, tools
+import os
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.files import copy, update_conandata, collect_libs
+from conan.tools.scm import Git
+from conans.errors import ConanException
 
 
 class SquallConan(ConanFile):
@@ -13,28 +19,43 @@ class SquallConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [False, True], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
-    generators = "cmake_find_package", "cmake_paths"
-    requires = "libcurl/7.87.0", "spdlog/1.11.0", "cli11/2.3.2"
-    build_requires = "catch2/3.3.1"
+    user = "worldforge"
+    generators = "CMakeToolchain", "CMakeDeps"
 
-    scm = {
-        "type": "git",
-        "url": "https://github.com/worldforge/squall.git",
-        "revision": "auto"
-    }
+    def requirements(self):
+        self.requires("libcurl/7.88.1")
+        self.requires("spdlog/1.11.0")
+        self.requires("cli11/2.3.2")
+        self.test_requires("catch2/3.3.1")
 
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.configure(source_folder=".")
-        return cmake
+    def export(self):
+        git = Git(self, self.recipe_folder)
+        try:
+            scm_url, scm_commit = git.get_url_and_commit()
+            update_conandata(self, {"sources": {"commit": scm_commit, "url": scm_url}})
+        except ConanException:
+            pass
+
+    def export_sources(self):
+        folder = os.path.join(self.recipe_folder, "../..")
+        copy(self, "*", folder, self.export_sources_folder, excludes=["*build*"])
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def layout(self):
+        self.folders.root = "../.."
+        cmake_layout(self)
 
     def build(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
-    def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
-
     def package(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
         cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = collect_libs(self)
