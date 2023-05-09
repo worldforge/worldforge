b0906c6b5958e14ea771bba65b30adcb5d61800e
Erik Ogenvik <erik@ogenvik.org>
2023-05-09 23:19:41 +0200

    Convert to Conan 2.

--- ogre/conanfile.py
+++ ogre/conanfile.py
@@ -1,7 +1,9 @@
 import os
 
-from conans import ConanFile, CMake, tools
-from conans.tools import os_info, SystemPackageTool
+from conan import ConanFile, tools
+from conan.tools.cmake import CMake, cmake_layout, CMakeToolchain
+from conan.tools.files import copy, collect_libs, get, replace_in_file, patch
+from conan.tools.system.package_manager import Apt
 
 
 class OgreConan(ConanFile):
@@ -10,7 +12,6 @@ class OgreConan(ConanFile):
     package_revision = ""
     version = "{0}{1}".format(upstream_version, package_revision)
 
-    generators = ['cmake_paths', 'cmake']
     settings = 'os', 'arch', 'compiler', 'build_type'
     options = {'shared': [True, False]}
     default_options = {"shared": False}
@@ -18,94 +19,92 @@ class OgreConan(ConanFile):
     license = 'MIT'
     description = ("Object-Oriented Graphics Rendering Engine (OGRE) "
                    "is a scene-oriented, real-time, 3D rendering engine.")
-    source_subfolder = 'source_subfolder'
     short_paths = False
     requires = ["bzip2/1.0.8",
                 "zlib/1.2.13",
                 "freetype/2.12.1",
-                "freeimage/3.18.0@worldforge/stable"]
+                "freeimage/3.18.0@worldforge"]
+    user = "worldforge"
+
+    def layout(self):
+        cmake_layout(self)
 
     def system_requirements(self):
-        if os_info.is_linux:
-            if os_info.with_apt:
-                installer = SystemPackageTool(default_mode="verify")
-                installer.install("libgl1-mesa-dev")
-                installer.install("libegl1-mesa-dev")
-                installer.install("libxrandr-dev")
+        Apt(self).install(["libgl1-mesa-dev", "libegl1-mesa-dev", "libxrandr-dev"])
 
     def source(self):
-        tools.get("https://github.com/OGRECave/ogre/archive/v{0}.tar.gz".format(self.upstream_version))
-        os.rename("ogre-{0}".format(self.upstream_version), self.source_subfolder)
+        get(self, "https://github.com/OGRECave/ogre/archive/v{0}.tar.gz".format(self.upstream_version), strip_root=True)
 
-    def build(self):
-        
-        tools.replace_in_file("{0}/CMakeLists.txt".format(self.source_subfolder), "include(OgreConfigTargets)", """include(OgreConfigTargets)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()
-""")
-        cmake = CMake(self)
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables['OGRE_BUILD_COMPONENT_PYTHON'] = 'OFF'
+        tc.variables['OGRE_BUILD_COMPONENT_JAVA'] = 'OFF'
+        tc.variables['OGRE_BUILD_SAMPLES'] = 'OFF'
+        tc.variables['OGRE_INSTALL_SAMPLES'] = 'OFF'
+        tc.variables['OGRE_INSTALL_SAMPLES_SOURCE'] = 'OFF'
+        tc.variables['OGRE_BUILD_TESTS'] = 'OFF'
+        tc.variables['OGRE_BUILD_TOOLS'] = 'OFF'
+        tc.variables['OGRE_BUILD_RENDERSYSTEM_GL'] = 'OFF'
+        tc.variables['OGRE_BUILD_RENDERSYSTEM_GLES2'] = 'OFF'
+        tc.variables['OGRE_BUILD_RENDERSYSTEM_GL3PLUS'] = "ON"
+        tc.variables['OGRE_BUILD_RENDERSYSTEM_D3D9'] = 'OFF'
+        tc.variables['OGRE_BUILD_RENDERSYSTEM_D3D11'] = 'OFF'
+        tc.variables['OGRE_INSTALL_DOCS'] = 'OFF'
+        tc.variables['OGRE_BUILD_PLUGIN_BSP'] = 'OFF'
+        tc.variables['OGRE_BUILD_PLUGIN_CG'] = 'OFF'
+        tc.variables['OGRE_BUILD_PLUGIN_FREEIMAGE'] = 'ON'
+        tc.variables['OGRE_BUILD_PLUGIN_EXRCODEC'] = 'OFF'
+        tc.variables['OGRE_BUILD_PLUGIN_OCTREE'] = 'OFF'
+        tc.variables['OGRE_BUILD_PLUGIN_PCZ'] = 'OFF'
+        tc.variables['OGRE_BUILD_PLUGIN_PFX'] = 'ON'
+        tc.variables['OGRE_BUILD_PLUGIN_STBI'] = 'OFF'
+        tc.variables['OGRE_BUILD_PLUGIN_DOT_SCENE'] = 'OFF'
+        tc.variables['OGRE_BUILD_DEPENDENCIES'] = 'OFF'
+        tc.variables['OGRE_CONFIG_ENABLE_ZIP'] = "OFF"
+        tc.variables['OGRE_BUILD_PLATFORM_APPLE_IOS'] = 'OFF'
+        tc.variables['OGRE_BUILD_COMPONENT_CSHARP'] = 'OFF'
+        tc.variables['OGRE_BUILD_COMPONENT_HLMS'] = 'OFF'
+        tc.variables['OGRE_BUILD_COMPONENT_PAGING'] = 'OFF'
+        tc.variables['OGRE_BUILD_COMPONENT_TERRAIN'] = 'ON'
+        tc.variables['OGRE_BUILD_COMPONENT_RTSHADERSYSTEM'] = 'ON'
+        tc.variables['OGRE_BUILD_COMPONENT_VOLUME'] = 'OFF'
+        tc.variables['OGRE_BUILD_COMPONENT_BITES'] = 'OFF'
+        tc.variables['OGRE_BUILD_COMPONENT_BULLET'] = 'OFF'
+        tc.variables['OGRE_BUILD_COMPONENT_PYTHON'] = 'OFF'
+        tc.variables['OGRE_BUILD_COMPONENT_PROPERTY'] = 'OFF'
+        tc.variables['OGRE_BUILD_COMPONENT_BITES'] = 'OFF'
+        tc.variables['OGRE_BUILD_COMPONENT_OVERLAY'] = 'ON'
+        tc.variables['OGRE_BUILD_COMPONENT_OVERLAY_IMGUI'] = 'OFF'
+        tc.variables['OGRE_CONFIG_THREAD_PROVIDER'] = 'std'
+        tc.variables['OGRE_BUILD_LIBS_AS_FRAMEWORKS'] = 'OFF'
+        tc.variables['OGRE_RESOURCEMANAGER_STRICT'] = 'true'
+        tc.variables['OGRE_NODELESS_POSITIONING'] = 'OFF'
+        tc.variables['OGRE_CONFIG_THREADS'] = '2'
+        tc.variables['CMAKE_TOOLCHAIN_FILE'] = 'conan_paths.cmake'
+        tc.variables['OGRE_STATIC'] = not self.options.shared
 
-        cmake.definitions['OGRE_BUILD_COMPONENT_PYTHON'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_COMPONENT_JAVA'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_SAMPLES'] = 'OFF'
-        cmake.definitions['OGRE_INSTALL_SAMPLES'] = 'OFF'
-        cmake.definitions['OGRE_INSTALL_SAMPLES_SOURCE'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_TESTS'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_TOOLS'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_RENDERSYSTEM_GL'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_RENDERSYSTEM_GLES2'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_RENDERSYSTEM_GL3PLUS'] = "ON"
-        cmake.definitions['OGRE_BUILD_RENDERSYSTEM_D3D9'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_RENDERSYSTEM_D3D11'] = 'OFF'
-        cmake.definitions['OGRE_INSTALL_DOCS'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_PLUGIN_BSP'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_PLUGIN_CG'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_PLUGIN_FREEIMAGE'] = 'ON'
-        cmake.definitions['OGRE_BUILD_PLUGIN_EXRCODEC'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_PLUGIN_OCTREE'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_PLUGIN_PCZ'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_PLUGIN_PFX'] = 'ON'
-        cmake.definitions['OGRE_BUILD_PLUGIN_STBI'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_PLUGIN_DOT_SCENE'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_DEPENDENCIES'] = 'OFF'
-        cmake.definitions['OGRE_CONFIG_ENABLE_ZIP'] = "OFF"
-        cmake.definitions['OGRE_BUILD_PLATFORM_APPLE_IOS'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_COMPONENT_CSHARP'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_COMPONENT_HLMS'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_COMPONENT_PAGING'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_COMPONENT_TERRAIN'] = 'ON'
-        cmake.definitions['OGRE_BUILD_COMPONENT_RTSHADERSYSTEM'] = 'ON'
-        cmake.definitions['OGRE_BUILD_COMPONENT_VOLUME'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_COMPONENT_BITES'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_COMPONENT_BULLET'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_COMPONENT_PYTHON'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_COMPONENT_PROPERTY'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_COMPONENT_BITES'] = 'OFF'
-        cmake.definitions['OGRE_BUILD_COMPONENT_OVERLAY'] = 'ON'
-        cmake.definitions['OGRE_BUILD_COMPONENT_OVERLAY_IMGUI'] = 'OFF'
-        cmake.definitions['OGRE_CONFIG_THREAD_PROVIDER'] = 'std'
-        cmake.definitions['OGRE_BUILD_LIBS_AS_FRAMEWORKS'] = 'OFF'
-        cmake.definitions['OGRE_RESOURCEMANAGER_STRICT'] = 'true'
-        cmake.definitions['OGRE_NODELESS_POSITIONING'] = 'OFF'
-        cmake.definitions['OGRE_CONFIG_THREADS'] = '2'
-        cmake.definitions['CMAKE_TOOLCHAIN_FILE'] = 'conan_paths.cmake'
-        cmake.definitions['OGRE_STATIC'] = not self.options.shared
-
-        if tools.os_info.is_windows:
-            cmake.definitions['OGRE_INSTALL_PDB'] = 'OFF'
+        if self.settings.os == "Windows":
+            tc.variables['OGRE_INSTALL_PDB'] = 'OFF'
         else:
-            cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = 'ON'
+            tc.variables['CMAKE_POSITION_INDEPENDENT_CODE'] = 'ON'
 
         # If we don't disable precompiled headers on Macos we get an error about "Objective-C was disabled in PCH file but is currently enabled"
         if self.settings.os == "Macos":
-            cmake.definitions['OGRE_ENABLE_PRECOMPILED_HEADERS'] = 'OFF'
+            tc.variables['OGRE_ENABLE_PRECOMPILED_HEADERS'] = 'OFF'
 
-        cmake.configure(source_folder=self.source_subfolder)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
-        if tools.os_info.is_windows:
+        if self.settings.compiler == "msvc":
             self.cpp_info.libdirs = ["lib", "lib/OGRE", "bin"]
         else:
             self.cpp_info.libdirs = ["lib", "lib/OGRE"]
@@ -119,11 +118,11 @@ conan_basic_setup()
                               "RenderSystem_GL3PlusStatic",
                               "OgreGLSupportStatic",
                               "OgreMainStatic"]
-        if tools.os_info.is_windows:
+        if self.settings.os == "Windows":
             self.cpp_info.system_libs = ["Opengl32"]
-        elif tools.os_info.is_linux:
+        elif self.settings.os == "Linux":
             self.cpp_info.system_libs = ["GL", "X11", "Xrandr"]
-        elif tools.os_info.is_macos:
+        elif self.settings.os == "Macos":
             self.cpp_info.system_libs = []
             self.cpp_info.frameworks = ["IOKit", "Cocoa", "Carbon", "OpenGL", "CoreVideo"]
 
@@ -132,6 +131,3 @@ conan_basic_setup()
                                      "include/OGRE/Terrain",
                                      "include/OGRE/MeshLodGenerator",
                                      "include/OGRE/RTShaderSystem"]
-
-    def package(self):
-        pass
