764323da8c24004734443064217b5a5d3cb2490b
Erik Ogenvik <erik@ogenvik.org>
2023-05-16 22:44:09 +0200

    Use Expat xml parser.

--- cegui/conanfile.py
+++ cegui/conanfile.py
@@ -43,7 +43,7 @@ class CeguiConan(ConanFile):
     def requirements(self):
         self.requires("freetype/2.13.0")
         self.requires("freeimage/3.18.0@worldforge")
-        self.requires("rapidxml/1.13")
+        self.requires("expat/2.5.0")
         self.requires("pcre/8.45")
 
     def generate(self):
@@ -69,13 +69,13 @@ class CeguiConan(ConanFile):
         tc.variables['CEGUI_BUILD_RENDERER_DIRECT3D11'] = 'OFF'
         tc.variables['CEGUI_BUILD_RENDERER_DIRECTFB'] = 'OFF'
         tc.variables['CEGUI_BUILD_LUA_GENERATOR'] = 'OFF'
-        tc.variables['CEGUI_BUILD_XMLPARSER_EXPAT'] = 'OFF'
+        tc.variables['CEGUI_BUILD_XMLPARSER_EXPAT'] = 'ON'
         tc.variables['CEGUI_BUILD_XMLPARSER_LIBXML2'] = 'OFF'
         tc.variables['CEGUI_BUILD_XMLPARSER_XERCES'] = 'OFF'
-        tc.variables['CEGUI_BUILD_XMLPARSER_RAPIDXML'] = 'ON'
+        tc.variables['CEGUI_BUILD_XMLPARSER_RAPIDXML'] = 'OFF'
         tc.variables['CEGUI_BUILD_XMLPARSER_TINYXML'] = 'OFF'
         tc.variables['CEGUI_BUILD_XMLPARSER_TINYXML2'] = 'OFF'
-        tc.variables['CEGUI_OPTION_DEFAULT_XMLPARSER'] = 'RapidXMLParser'
+        tc.variables['CEGUI_OPTION_DEFAULT_XMLPARSER'] = 'ExpatXMLParser'
         if not self.options.shared:
             tc.variables['CEGUI_BUILD_STATIC_CONFIGURATION'] = 'ON'
             tc.variables['CEGUI_BUILD_STATIC_FACTORY_MODULE'] = 'ON'
