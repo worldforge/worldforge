b9ade752dc53441fa8ccacfe77b404a6875d621a
Erik Ogenvik <erik@ogenvik.org>
2023-05-31 16:18:29 +0200

    Use Conan install location in test.

--- cpython/test_package/conanfile.py
+++ cpython/test_package/conanfile.py
@@ -1,13 +1,21 @@
 import os
 
 from conan import ConanFile
-from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.build import can_run
 
 
 class TestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps", "CMakeToolchain"
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+
+        tc = CMakeToolchain(self)
+        tc.preprocessor_definitions["PYTHONHOME"] = "\"{}\"".format(self.dependencies["cpython"].package_folder)
+        tc.generate()
+
 
     def requirements(self):
         self.requires(self.tested_reference_str)
--- cpython/test_package/py3/test_package.c
+++ cpython/test_package/py3/test_package.c
@@ -4,6 +4,7 @@
 int
 main(int argc, char *argv[])
 {
+    setenv("PYTHONHOME", PYTHONHOME, 1);
     wchar_t *program = Py_DecodeLocale(argv[0], NULL);
     if (program == NULL) {
         fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
