ef51b38a18e33667f6171c749949880d9b29fac0
Erik Ogenvik <erik@ogenvik.org>
2023-10-31 16:11:41 +0100

    Convert to using monorepo layout.
    Everything builds, but there's a need for cleanup in general.

---
+++ conanfile.py
@@ -0,0 +1,88 @@
+import os
+
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout, CMakeDeps, CMakeToolchain
+from conan.tools.microsoft import is_msvc
+
+
+class Worldforge(ConanFile):
+    version = "0.1.0"
+    package_type = "application"
+    settings = "os", "arch", "compiler", "build_type"
+    url = "https://github.com/worldforge"
+    homepage = "https://www.worldforge.org"
+    name = "Worldforge"
+    license = "GPL-3.0-or-later"
+    author = "Erik Ogenvik <erik@ogenvik.org>"
+
+    options = {
+        "with_client": [True, False],
+        "with_server": [True, False],
+    }
+
+    default_options = {
+        "with_client": True,
+        "with_server": True,
+        # Skipped because we had issues building xz_utils, which is used by libunwind
+        'sdl/*:libunwind': False,
+        # We got build errors with X11 on OpenSuSE, so disable for now. Unclear what the X11 support entails.
+        'pulseaudio/*:with_x11': False,
+        # Unclear why the pulseaudio client lib needs openssl...
+        'pulseaudio/*:with_openssl': False,
+        # We're getting compilation errors on Ubuntu 22.04 with "mpg123" so we'll disable that. Might want to enable
+        # again when we enable better sound support.
+        'libsndfile/*:with_mpeg': False
+    }
+
+    def requirements(self):
+        self.requires("bullet3/2.89")
+        self.requires("libsigcpp/3.0.7")
+        self.requires("libcurl/8.2.1")
+        self.requires("spdlog/1.12.0")
+        self.requires("cli11/2.3.2")
+
+        self.requires("zlib/1.2.13")
+        self.requires("bzip2/1.0.8")
+
+        if self.options.with_client:
+            self.requires("cegui/0.8.7@worldforge")
+            self.requires("ogre/13.4.2@worldforge")
+            self.requires("sdl/2.28.3")
+            self.requires("lua/5.3.6")
+            self.requires("libxml2/2.10.4", override=True)
+
+        if self.options.with_server:
+            self.requires("worldforge-worlds/0.1.0@worldforge")
+            self.requires("libgcrypt/1.8.4")
+            self.requires("sqlite3/3.42.0")
+            self.requires("readline/8.1.2@worldforge")
+            self.requires("cpython/3.10.0@worldforge")
+            self.requires("boost/1.81.0")
+
+        if not is_msvc(self):
+            self.requires("libxdg-basedir/1.2.3@worldforge")
+
+        self.requires("libcap/2.69", override=True)
+
+        self.test_requires("cppunit/1.15.1")
+        self.test_requires("catch2/3.4.0")
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        # OGRE provides its own CMake files which we should use
+        deps.set_property("ogre", "cmake_find_mode", "none")
+        deps.generate()
+
+        tc = CMakeToolchain(self)
+        # We need to do some stuff differently if Conan is in use, so we'll tell the CMake system this.
+        tc.variables["CONAN_FOUND"] = "TRUE"
+        tc.variables["PYTHON_IS_STATIC"] = "TRUE"
+        # The default CMake FindPython3 component will set the Python3_EXECUTABLE, which is then used in Cyphesis. Therefore do this here.
+        tc.variables["Python3_EXECUTABLE"] = os.path.join(self.dependencies["cpython"].package_folder, "bin/python")
+        tc.variables["WORLDFORGE_WORLDS_SOURCE_PATH"] = os.path.join(
+            self.dependencies["worldforge-worlds"].package_folder, "worlds")
+        tc.preprocessor_definitions["PYTHONHOME"] = "\"{}\"".format(self.dependencies["cpython"].package_folder)
+        tc.generate()
+
+    def layout(self):
+        cmake_layout(self)
