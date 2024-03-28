ccbc5bd59487478840ed81ecc8b439f2a7abde26
Erik Ogenvik <erik@ogenvik.org>
2024-03-28 23:32:49 +0100

    Bump SDL.

--- conanfile.py
+++ conanfile.py
@@ -53,7 +53,7 @@ class Worldforge(ConanFile):
         if self.options.with_client:
             self.requires("cegui/0.8.7@worldforge")
             self.requires("ogre/14.2.3@worldforge")
-            self.requires("sdl/2.28.5")
+            self.requires("sdl/2.30.1")
             self.requires("lua/5.3.6")
             self.requires("vorbis/1.3.7")
 
