391cd9d7923be95e7ecf244526927371ae3c6c17
Erik Ogenvik <erik@ogenvik.org>
2025-01-02 13:13:15 +0100

    Bump OGRE.

--- tools/conan-packages/ogre/conanfile.py
+++ tools/conan-packages/ogre/conanfile.py
@@ -11,7 +11,7 @@ from conan.tools.system.package_manager import Apt
 
 class OgreConan(ConanFile):
     name = 'ogre'
-    upstream_version = "14.3.1"
+    upstream_version = "14.3.2"
     package_revision = ""
     version = "{0}{1}".format(upstream_version, package_revision)
 
